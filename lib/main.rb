c8ff7b57a5b2d840d3a0c574dd39ce61e5b65b83
SampabloC <sampablo-jisc@outlook.com>
2021-06-14 19:05:30 -0500

    Pruebas unitarias Avance
    Se crearon las pruebas para validar creación de la matriz, impresión de la matriz y validar la condición de no tener vida en los bordes
    
    Commit para tener un respaldo de lo que esta funcionando
    
    Co-authored-by: xFerchoVJ<fer_elsword@hotmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -1,51 +1,69 @@
-#Codigo para juego de la vida
+# frozen_string_literal: true
+
+# Codigo para juego de la vida
 class Matriz
   def initialize(ancho, largo)
     @ancho = ancho
     @largo = largo
     @grid = []
   end
-
+  
   def build
-      @ancho.times do |_i|
-        @grid.push([])
-      end
-      llenar
+    @ancho.times do |_i|
+      @grid.push([])
+    end
+    llenar
   end
 
   def llenar
-    @ancho.times do |i|
-        @largo.times do |_x|
-            if rand(2).zero?
-                @grid.push(' ° ')
-            else
-                @grid.push(' * ')
-            end
+    @grid.each do |i|
+      @largo.times do |_x|
+        if rand(2).zero?
+          i.push(' ° ')
+        else
+          i.push(' * ')
         end
+      end
     end
     @grid
-  end
-
-  def suma(valor)
-    valor*valor
   end
 end
 
 class Show < Matriz
-	def initialize(ancho, largo, grid)
-		super(ancho, largo)
-		@grid = grid
-	end
+  def initialize(ancho, largo, grid)
+    super(ancho, largo)
+    @grid = grid
+  end
 
-	def show
-		(0..@ancho - 1).each do |i|
-			cadena = ""
-			(0..@largo).each do |x|
-				cadena += @grid[i][x]
-			end
-			puts cadena
-		end
-	end
+  def show
+    (0..@ancho - 1).each do |i|
+      cadena = ''
+      (0..@largo - 1).each do |x|
+        cadena += @grid[i][x]
+      end
+      puts cadena
+    end
+  end
+end
 
-	
+class Condiciones < Matriz
+  def initialize(ancho, largo, grid)
+    super(ancho, largo)
+    @grid = grid
+  end
+
+  #Matar celulas en las orillas
+  def matar_bordes
+    (0..@ancho - 1).each do |i|
+      (0..@largo).each do |x|
+        @grid[i][x] = ' ° ' if i.zero? || i == @ancho - 1 || x.zero? || x == @largo - 1
+        
+      end
+    end
+  end
+  
+  # #Contar los vecinos de las celulas
+  # def conteo_vecinos
+    
+  # end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,29 +1,45 @@
+# frozen_string_literal: true
+
 require 'main'
 
 RSpec.describe Matriz do
-    context "prueba" do #Prueba para entender el funcionamiento de la tecnolgia...
-        it "suma" do
-            juego = Matriz.new(5,6)
-         expect(juego.suma(5)).to eq(25)
-        end
+  # #  ejemplo sobre uso de rspec
+  # context 'prueba' do # Prueba para entender el funcionamiento de la tecnolgia...
+  #   it 'suma' do
+  #     juego = Matriz.new(5, 6)
+  #     expect(juego.suma(5)).to eq(25)
+  #   end
+  # end
+  
+  let(:matriz) { Matriz.new(5,6) }
+  let(:matrix) { matriz.build}
+
+  context 'validar creacion de matriz' do # Verificar que se cree la matriz
+    it 'creacion' do
+      expect(matrix) != [] # condicion para validar que la matriz no esta vacia
     end
+  end
 
-     context "creacion de matriz" do #Verificar que se cree la matriz
-         it "prueba" do
-             juego = Matriz.new(5,6)
-             expect(juego.llenar()) != [] #condicion para validar que la matriz no esta vacia
-         end
-     end
-    
-   # context "Analizar muertas"
-#     it "setup" do 
-#         @juego = Kata.new(5,6)
-#         expect(@juego.suma(6)).to eq(25)
-#     end
+  context 'prueba de muestra de matriz' do
+    it "muestra" do
+      juego = Show.new(5, 6, matrix)
+      expect(juego.show) != ''
+    end
+  end
 
-# #     it exercise do
-        
-# #     end
-
-    
-end        
+  context 'validar bordes muertos'do
+    it 'Muerto' do
+      muerto = Condiciones.new(5,6,matrix)
+      muerto.matar_bordes
+      5.times do |i|
+        6.times do |x|
+          if i == 0 || i == 5 || x == 0 || x == 6
+            #it 'Muerto' do
+            expect(matrix[i][x]).to eq' ° '
+            #end 
+          end
+        end
+      end
+    end
+  end
+end
